006500d75eb7a78f990a823a529e946e156a7e56
Akhil Babu <akhilbabu123@gmail.com>
2025-12-30 13:35:03 +0530

    fibonacci series

---
+++ day-1/exercise-3/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	a,b := 0,1
+
+	// for i := 0; i < 10; i++{
+	// 	fmt.Println(a)
+	// 	a,b = b,a+b
+	// }
+
+	for range 10{
+		fmt.Println(a)
+		a,b = b, a+b
+	}
+}
