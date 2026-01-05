52d1743a6a4e6013f244bffa11bc5b0e16e67d74
Akhil Babu <akhilbabu123@gmail.com>
2026-01-05 20:12:19 +0530

    select-statement

---
+++ week-1/day-5/select-statement/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main(){
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go func(){
+		time.Sleep(2 * time.Second)
+		ch1 <- "Massage from ch1"
+		
+	}()
+
+	go func(){
+		time.Sleep(1 * time.Second)
+		ch2 <- "Massage fron ch2"
+	}()
+
+	select{
+	case msg := <-ch1:
+		fmt.Println("c1 massage:",msg)
+	case msg := <-ch2:
+		fmt.Println("c2 massage:",msg)
+	}
+}
