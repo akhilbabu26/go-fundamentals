4a5356e1debd0426a613a09bcfe68984b869334f
Akhil Babu <akhilbabu123@gmail.com>
2026-01-15 11:21:17 +0530

    day4-valid-parentheses-task

---
+++ week-2/dsa/day-4/exercise/Valid-Parentheses/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func isValid(s string)bool{
+	stack := []rune{}
+
+	for _,ch := range s{
+		if ch == '(' || ch == '{' || ch == '['{
+			stack = append(stack, ch)
+		}else{
+			if len(stack) == 0{
+				return false
+			}
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			
+			if (ch == ')' && top != '(') || 
+			(ch == '}' && top != '{') ||
+			(ch == ']' && top != '['){
+				return false
+			}
+		}
+	}
+	return len(stack) == 0
+}
+
+func main(){
+	result := isValid("(]")
+
+	fmt.Println(result)
+}
